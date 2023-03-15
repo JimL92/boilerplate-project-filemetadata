a78b6c9b809611d555175a01da694da791dba524
JimL92 <sundownglow2991@gmail.com>
2023-03-15 22:02:12 +0000

    initial freecodecamp file metadata microservice project commit

--- index.js
+++ index.js
@@ -1,19 +1,29 @@
 var express = require('express');
 var cors = require('cors');
+var multer = require('multer');
+let bodyParser = require('body-parser');
 require('dotenv').config()
 
 var app = express();
 
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
 
-
-
+//api/fileanalyse
+const upload = multer({ dest: './public/data/uploads/' })
+app.post('/api/fileanalyse', upload.single('upfile'), function (req, res) {
+  res.json({name: req.file.originalname, type: req.file.mimetype, size: req.file.size})
+   // req.file is the name of your file in the form above, here 'upfile'
+   // req.body will hold the text fields, if there were any 
+   console.log(req.file, req.body)
+});
 const port = process.env.PORT || 3000;
 app.listen(port, function () {
   console.log('Your app is listening on port ' + port)
